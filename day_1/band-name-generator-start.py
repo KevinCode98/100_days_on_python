c8afc1e9da3d191a7361e14f00e125d3454fb169
KevinCode98 <kevincode98@gmail.com>
2022-07-11 22:03:07 -0500

    Day 1 -> Finish at: 11/07/22

---
+++ day_1/band-name-generator-start.py
@@ -0,0 +1,14 @@
+# 1. Create a greering for your program.
+print("Welcome to my program")
+
+# 2. Ask the user for the city that they grew up in.
+user_city = input("Where were you born?\n")
+
+# 3. Ask the user for the name of a pet.
+pet_name = input("What is your pet's name?\n")
+
+# 4. Cobine the name of their city and pet and show them their band name.
+print("The name of the band:", user_city, pet_name)
+
+# 5. Make Sure the input coursor shows on a new line, see the example at:
+#   https://band-name-generator-end.appbrewery.repl.run/
---
+++ day_1/day-1-2-exercise.py
@@ -0,0 +1,6 @@
+# Fix the code below 
+
+print("Day 1 - String Manipulation")
+print('String Concatenation is done with the "+" sing.')
+print('e.g. print("Hello " + "world")')
+print ("New lines canbe created with a backslash and n.")
